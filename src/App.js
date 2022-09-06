3f2e6a3f8d604d4d56d57970a269ea1879f1cde2
Erik Kjosbakken <erik@skyetec.no>
2022-09-06 09:44:31 +0200

    Added api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const text = await( await fetch(`/api/message`)).text();
+      setData(text);
+    })();
+  }, [])
+
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello! <br /> {data}</div>;
 }
 
 export default App;
